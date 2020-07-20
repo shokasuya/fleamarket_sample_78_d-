59d5dfd00362cf61117f7823ef59190197fdd86a
takuro-s7 <66065496+takuro-s7@users.noreply.github.com>
2020-07-20 18:55:54 +0900

    category-list-create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 
-//= require turbolinks
+
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
+//= require turbolinks
 //= require_tree .
