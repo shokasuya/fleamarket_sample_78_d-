5b27c58bed60207daca645316c97f2a47fe137ca
kaz-14 <66046956+kaz-14@users.noreply.github.com>
2020-07-20 18:16:02 +0900

    fix-add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,7 @@ class ProductsController < ApplicationController
       @product.images.new
       @parent = Category.where(ancestry: nil)
     else
+      @parent = Category.where(ancestry: nil)
       redirect_to root_path
     end
   end
@@ -27,7 +28,9 @@ class ProductsController < ApplicationController
     if params[:product][:images_attributes] && @product.save
       redirect_to root_path
     else
-      redirect_to root_path
+      @parent = Category.where(ancestry: nil)
+      flash.now[:alert] = '未入力項目があります'
+      render :new
     end
   end
 
